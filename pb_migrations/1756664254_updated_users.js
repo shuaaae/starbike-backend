c73b5f874e7116f3d728ac86dc0503f76a936c99
shuaaae <joshuagodalle210904@gmail.com>
2025-09-01 02:36:06 +0800

    trenta

---
+++ pb_migrations/1756664254_updated_users.js
@@ -0,0 +1,50 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "passwordAuth": {
+      "enabled": false
+    }
+  }, collection)
+
+  // update field
+  collection.fields.addAt(3, new Field({
+    "exceptDomains": null,
+    "hidden": false,
+    "id": "email3885137012",
+    "name": "email",
+    "onlyDomains": null,
+    "presentable": false,
+    "required": true,
+    "system": true,
+    "type": "email"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "passwordAuth": {
+      "enabled": true
+    }
+  }, collection)
+
+  // update field
+  collection.fields.addAt(3, new Field({
+    "exceptDomains": null,
+    "hidden": false,
+    "id": "email3885137012",
+    "name": "email",
+    "onlyDomains": null,
+    "presentable": false,
+    "required": false,
+    "system": true,
+    "type": "email"
+  }))
+
+  return app.save(collection)
+})
